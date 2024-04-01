c6956a5b63ec750b21b3ceffc724996f1a93e059
meher <baisameher@gmail.com>
2024-04-01 12:02:26 +0530

    app testing

---
+++ src/main/java/com/helloworld/myfirstproject/MyFirstProjectApplication.java
@@ -0,0 +1,14 @@
+package com.helloworld.myfirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstProjectApplication.class, args);
+	}
+
+}
+
